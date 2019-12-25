84e3a2eca2f07d686b79a1524befaf92841f356a
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2019-12-25 18:24:00 +0530

    Add files via upload

---
+++ Loops/Star_Pattern_4.py
@@ -0,0 +1,8 @@
+a=input("Enter the number of rows : ")
+for i in range(1,int(a)+1):
+    for j in range(0,int(a)-i):
+        print(" ",end="")
+    for k in range(1,i+1):
+        print("*",end="")
+    print("\n")
+   
