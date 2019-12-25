3683f839a666742cef5f119a5682dd4c05cc104e
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2019-12-25 10:07:09 +0530

    Add files via upload

---
+++ Loops/Prime_Numbers_between_1_to_n.py
@@ -0,0 +1,13 @@
+a=input("Enter a number : ")
+c=[]
+if (int(a)==1):
+    print("No prime numbers between 1 and " + str(a))
+else:
+    for i in range(1,int(a)+1):
+        b=[]
+        for j in range(1,int(a)+1):
+            if (i%j==0):
+                b.append(j)
+        if (len(b)==2):
+            c.append(i)
+    print(c)
