0393a5e02584b04077176b4dec695727894d1513
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2019-12-27 11:46:17 +0530

    Add files via upload

---
+++ Conditionals/Uppercase_Or_Lowercase.py
@@ -0,0 +1,7 @@
+a=input("Enter a character : ")
+for i in range(65,91):
+    if a==chr(i):
+        print(a + " is in uppercase.")
+for i in range(97,123):
+    if a==chr(i):
+        print(a+ " is in lowercase.")
