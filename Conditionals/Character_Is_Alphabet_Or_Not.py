f30e38892afc55052e4b46f88293c5a8cf608ef8
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2019-12-27 11:44:00 +0530

    Add files via upload

---
+++ Conditionals/Character_Is_Alphabet_Or_Not.py
@@ -0,0 +1,8 @@
+a=input("Enter a character : ")
+if (a>='a' and a<='z'):
+     print("The character entered is an alphabet")
+elif (a>='A' and a<='Z'):
+     print("The character entered is an alphabet")
+else:
+     print("The character entered is not an alphabet")
+    
