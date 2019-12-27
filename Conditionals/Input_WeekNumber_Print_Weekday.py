973114bd6f746fee393999e27f12f1340a218d82
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2019-12-27 11:46:45 +0530

    Add files via upload

---
+++ Conditionals/Input_WeekNumber_Print_Weekday.py
@@ -0,0 +1,17 @@
+a=int(input("Enter Week Number : "))
+if a==1:
+    print("Monday")
+elif a==2:
+    print("Tuesday")
+elif a==3:
+    print("Wednesday")
+elif a==4:
+    print("Thursday")
+elif a==5:
+    print("Friday")
+elif a==6:
+    print("Saturday")
+elif a==7:
+    print("Sunday")
+else:
+    print("Enter a valid Week Number.")
