8f25c4381bc1671fc1a591a2e985cc0365feb3bd
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2019-12-27 20:36:59 +0530

    Add files via upload

---
+++ Conditionals/Triangle_Valid_Or_Not.py
@@ -0,0 +1,8 @@
+a=input("Enter angle1 : ")
+b=input("Enter angle2 : ")
+c=input("Enter angle3 : ")
+d=float(a)+float(b)+float(c)
+if d==180:
+    print("Triangle is valid.")
+else:
+    print("Triangle is not valid.")
