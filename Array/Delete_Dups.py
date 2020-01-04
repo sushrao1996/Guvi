48036d8053c42ec005c7dde2be72fd76dcdbf8fa
sushrao1996 <59138399+sushrao1996@users.noreply.github.com>
2020-01-04 08:20:13 +0530

    Add files via upload

---
+++ Array/Delete_Dups.py
@@ -0,0 +1,13 @@
+import array as ar
+a=ar.array('i',[])
+b=ar.array('i',[])
+size=int(input("Enter the size of the array : "))
+print("Enter the elements : ")
+for i in range(size):
+    a.append(int(input()))
+for i in a:
+    if i not in b:
+        b.append(i)
+print("Array after removing the duplicate elements : ")
+for i in b:
+    print(i,end=" ")
